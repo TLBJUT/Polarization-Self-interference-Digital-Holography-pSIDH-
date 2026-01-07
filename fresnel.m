205c5e533b3e4e44639b218180df022a4923b8c4
TLBJUT <110218985+TLBJUT@users.noreply.github.com>
2026-01-07 09:30:36 +0800

    Update fresnel.m

--- fresnel.m
+++ fresnel.m
@@ -15,12 +15,11 @@
 % 
 % # [1] T. Man, Y, Wan, W. Yan, et al., "Adaptive optics via self-interference digital holography
 % for non-scanning three-dimensional imaging in biological samples," Biomedical Optics Express, 9(6):2614, 2018.
-% # https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/models/networks.py
 % #
 % # [2] T. Man, W. Zhang, L. Zhang, et al., "Scanning-free three-dimensional fluorescent dipoles imaging by polarization 
 % self-interference digital holography (pSIDH)," DOI
 % 10.48550/arXiv.2504.10772, 2025
-% # https://github.com/csleemooo/Deep_learning_based_on_parameterized_physical_forward_model_for_adaptive_holographic_imaging
+% # https://github.com/TLBJUT/Polarization-Self-interference-Digital-Holography-pSIDH-.git
 % 
 % #==============================================================================
 function [f1,dx1,dy1,x1,y1] = fresnel(f0,M,N,dx0,dy0,z,lambda)
@@ -35,4 +34,5 @@ H=exp(-i*pi*lambda*z*(u.^2+v.^2));
 f1=ifft2(fft2(f0).*H);  
 dx1=dx0;dy1=dy0;
 x1=ones(N,1)*[-M/2:M/2-1]*dx1;                         
+
 y1=[-N/2:N/2-1]'*ones(1,M)*dy1;
